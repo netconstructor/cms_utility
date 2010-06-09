a04eb1d663a4b843e50d0ad0a52fed05a8ebde15
Don Kukral <donk@debian.localdomain>
2010-06-09 14:11:01 -0400

    broke out localized settings from settings file

---
+++ localsettings.py
@@ -0,0 +1,8 @@
+import os
+
+ROOT_PATH = os.path.dirname(__file__)
+ROOT_URL = 'http://debian:8000/'
+ANTIWORD = '/usr/bin/antiword'
+UNRTF = '/usr/bin/unrtf --text'
+TEMP_FILES = '/tmp/temp_files'
+
