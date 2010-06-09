46d2a2caf642fee68673a861827dd5c0cb73dd13
Don Kukral <donk@debian.localdomain>
2010-06-09 19:38:35 -0400

    added hints

--- localsettings.py
+++ localsettings.py
@@ -1,7 +1,7 @@
 import os
 
 ROOT_PATH = os.path.dirname(__file__)
-ROOT_URL = 'http://debian:8000/'
+ROOT_URL = 'http://debian:8000/' # to run local make this: http://localhost:8000/
 ANTIWORD = '/usr/bin/antiword'
 UNRTF = '/usr/bin/unrtf --text'
 TEMP_FILES = '/tmp/temp_files'
