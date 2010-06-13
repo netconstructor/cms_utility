04d81902058d23c8a08a2542ded67e422d5bcda3
Don Kukral <donk@debian.localdomain>
2010-06-13 19:39:16 -0400

    added configurations for apps.joeboydston.com

--- localsettings.py
+++ localsettings.py
@@ -1,8 +1,16 @@
 import os
+from socket import gethostname
 
 ROOT_PATH = os.path.dirname(__file__)
-ROOT_URL = 'http://debian:8000/' # to run local make this: http://localhost:8000/
+ROOT_URL = 'http://localhost:8000/' 
 ANTIWORD = '/usr/bin/antiword'
 UNRTF = '/usr/bin/unrtf --text'
 TEMP_FILES = '/tmp/temp_files'
 
+# for prod
+if gethostname() == 'www3.news-apps.com':
+    ROOT_URL = 'http://apps.joeboydston.com/cms_utility/'
+elif gethostname() == 'debian':
+    ROOT_URL = 'http://debian:8000/'
+else:
+    pass
