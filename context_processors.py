7649b606c5eb3946584451747be46d050e9d2592
Don Kukral <donk@debian.localdomain>
2010-06-13 19:40:15 -0400

    added num user and doc count

--- context_processors.py
+++ context_processors.py
@@ -1,4 +1,10 @@
 from django.conf import settings 
+from publisher.models import Settings, DocumentsProcessed
+
 
 def add_settings(request):
-    return {'settings': settings }
+    
+    number_of_users = Settings.objects.count()
+    doc_count = DocumentsProcessed.objects.get(id=1)
+    return {'settings': settings, 'num_users': number_of_users, 
+        'doc_count': doc_count.total }
