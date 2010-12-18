f041ff786bc89632f36aca83c2faaa376dc9c2cd
Don Kukral <don@kukral.org>
2010-12-18 12:14:21 -0500

    login decorator and cleanup

--- publisher/views.py
+++ publisher/views.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
 
 from models import Settings, LAYOUTS, DIR_STRUCTURES, DocumentsProcessed
 from forms import FileUploadForm, SettingsForm
@@ -17,7 +18,8 @@ def index(request):
     config = lookup_settings(request)
     return direct_to_template(request, 'index.html', 
         {'config': config,})
-    
+
+@login_required
 def cms_settings(request):
     config = None
     message = get_messages(request)
@@ -52,6 +54,7 @@ def clear_cms_settings(request):
     request.session['message'] = "Settings Cleared."
     return HttpResponseRedirect(settings.ROOT_URL+'options/')
         
+@login_required
 def single_upload(request):
     posts = categories = pub_date = image = None
     config = lookup_settings(request)
@@ -78,10 +81,11 @@ def single_upload(request):
         'form': form, 'posts': posts, 'pub_date': pub_date, 
         'layout': 'default', })
     
+@login_required
 def batch_upload(request):
-    post = form = categories = pub_date = rv_posts = None
+    posts = form = categories = pub_date = rv_posts = None
     config = lookup_settings(request)
-    
+
     if config:
         blog = CMSUtility(config)
         categories = blog.get_categories()
@@ -97,12 +101,13 @@ def batch_upload(request):
             form = blog.form()
             posts = None
     else:
-        post = form = categories = pub_date = None
+        posts = form = categories = pub_date = None
         
     return direct_to_template(request, 'batch_upload.html', 
         {'config': config, 'categories': categories,
         'form': form, 'posts': posts, 'pub_date': pub_date, })
         
+@login_required
 def single_upload_file_info(request):
     posts = form = categories = pub_date = None
     config = lookup_settings(request)
@@ -129,8 +134,9 @@ def single_upload_file_info(request):
         'form': form, 'posts': posts, 'pub_date': pub_date, 
         'layouts': LAYOUTS})
         
+@login_required
 def batch_upload_hierarchy(request):
-    post = form = categories = pub_date = rv_posts = None
+    posts = form = categories = pub_date = rv_posts = None
     config = lookup_settings(request)
 
     if config:
@@ -145,49 +151,11 @@ def batch_upload_hierarchy(request):
                 blog.post_stories()
                 posts = blog.get_links()
 
-#                post_file = form.cleaned_data['post_file']
-#                layout = form.cleaned_data['layout']
-#                dir_structure = form.cleaned_data['dir_structure']
-#                posts = process_zip_file(post_file, layout=layout, 
-#                    dir_structure=dir_structure)
-#                    
-#                pub_date = form.cleaned_data['date']
-#                dateCreated = xmlrpclib.DateTime(
-#                    time.mktime(pub_date.timetuple()))
-#                mt_keywords = form.cleaned_data['tags']
-#                category = form.cleaned_data['category']
-
-
-#                rv_posts = []
-#                for cms_post in posts:
-#                    if 'category' in cms_post:
-#                        post_category = [cms_post['category']]
-#                        cat_id = 0
-#                        for cat in categories:
-#                            if cat['categoryName'] == cms_post['category']:
-#                                cat_id = int(cat['categoryId'])
-#                        if not cat_id:
-#                            cat_id = blog.new_category(
-#                                {'name': cms_post['category'],
-#                                'slug': cms_post['category'].replace(' ', '-'),
-#                                'parent_id': 0, 
-#                                'description': cms_post['category']})
-#                        cms_post['categories'] = [cms_post['category']]
-#                    else: 
-#                        cms_post['categories'] = [category]
-#                        
-#                    cms_post['mt_keywords'] = mt_keywords
-#                    cms_post['dateCreated'] = dateCreated
-#                    
-#                    rv = new_post(blog, cms_post)
-#                    if rv:
-#                        post = blog.get_post(rv)            
-#                        rv_posts.append(post)
         else:
             form = blog.form()
             posts = None
     else:
-        post = form = categories = pub_date = None
+        posts = form = categories = pub_date = None
         
     return direct_to_template(request, 'batch_upload_hierarchy.html', 
         {'config': config, 'categories': categories,
